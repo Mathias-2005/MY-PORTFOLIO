938f65e1758d09aac27c3379ab17efa2aad4dbd1
Mathias <mathiasmaillydepinho@gmail.com>
2026-02-12 15:36:34 +0100

    add error page

--- src/layout/App.jsx
+++ src/layout/App.jsx
@@ -6,11 +6,11 @@ import Footer from '../components/Footer';
 function App() {
 
     return (
-        <div>
+        <div className='app-container'>
             <header>
                 <Header />
             </header>
-            <main>
+            <main className='main-content'>
                 <Outlet />
             </main>
             <footer>
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,19 @@
-function Error(){
+import { Link } from "react-router-dom";
+import '../components/Error.scss';
 
+function Error() {
+
+    return (
+        <>
+            <div className="error">
+                <p className="error__404">404</p>
+                <p className="error__404-p">Oups! La page que vous demandez n'existe pas.</p>
+                <Link to={"/"}>
+                    <p className="error__404-a">Retourner sur la page d'accueil</p>
+                </Link>
+            </div>
+        </>
+    )
 }
 
 export default Error;
